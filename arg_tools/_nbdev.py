4014ac61098fcd5c8e4ddf656af204e2232e9dbf
ezrafollower <ezrafollower@gmail.com>
2022-06-01 14:26:08 +0800

    edit hide and export tag

--- arg_tools/_nbdev.py
+++ arg_tools/_nbdev.py
@@ -2,9 +2,11 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"gdown_unzip": "00_utils.ipynb"}
+index = {"gdown_unzip": "00_utils.ipynb",
+         "camera_projection": "01_camera_projection.ipynb"}
 
-modules = ["utils.py"]
+modules = ["utils.py",
+           "camera_projection.py"]
 
 doc_url = "https://ARG-NCTU.github.io/arg_tools/"
 
