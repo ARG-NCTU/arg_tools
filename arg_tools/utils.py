c239958be9cc7c51469d1efa904c3ba6568bccb2
austin2408 <sdnd93612@gmial.com>
2022-06-02 13:44:06 +0800

    update

--- arg_tools/utils.py
+++ arg_tools/utils.py
@@ -28,15 +28,15 @@ def gdown_unzip(id, filename):
 def pose_dis(pose_1, pose_2):
     """Compute distance between pose_1 and pose_2
     """
-    x = pose_1.position.x - pose_2.position.x
-    y = pose_1.position.y - pose_2.position.y
-    z = pose_1.position.z - pose_2.position.z
+    x = pose_1.target_pose.position.x - pose_2.target_pose.position.x
+    y = pose_1.target_pose.position.y - pose_2.target_pose.position.y
+    z = pose_1.target_pose.position.z - pose_2.target_pose.position.z
 
     dis = math.sqrt(x**2+y**2+z**2)
 
     return dis
 
-def waypoint(current_pose, target_pose):
+def waypoint(current_pose, Target_pose):
     """Generate a list of way points from current pose to target pose
 
     Input : current pose, target pose
@@ -49,12 +49,12 @@ def waypoint(current_pose, target_pose):
 
     # threshold : distance between sub_pose and target_pose = 0.05 meter
     while pose_dis(current_pose, target_pose) > 0.05:
-        sub_pose.position.x = (sub_pose.position.x + target_pose.position.x)*factor
-        sub_pose.position.y = (sub_pose.position.y + target_pose.position.y)*factor
-        sub_pose.position.z = (sub_pose.position.z + target_pose.position.z)*factor
+        sub_pose.target_pose.position.x = (sub_pose.target_pose.position.x + Target_pose.target_pose.position.x)*factor
+        sub_pose.target_pose.position.y = (sub_pose.target_pose.position.y + Target_pose.target_pose.position.y)*factor
+        sub_pose.target_pose.position.z = (sub_pose.target_pose.position.z + Target_pose.target_pose.position.z)*factor
 
         waypoint_list.append(sub_pose.copy())
 
-    waypoint_list.append(target_pose)
+    waypoint_list.append(Target_pose)
 
     return waypoint_list
